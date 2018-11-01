8643bf2654e8f28f877e8890e93404c84362b91f
Nistor-Sergiu-Cosmin <nistorsergiu2112@gmail.com>
2018-11-02 00:45:54 +0200

    Everything working correctly

--- src/component/ListItem.js
+++ src/component/ListItem.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 
 export default class ListItem extends Component {
   render() {
-    return (<li className="listItem" onClick={() => this.props.handleListItemClick(this.props)}>
+    return (<li tabIndex="0" className="listItem" onClick={() => this.props.handleListItemClick(this.props)}>
       <img src={this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} alt={this.props.categories[0].name}/>
       {this.props.name}
     </li>)
